f946fb8461980e61da761842b2c2ea91fcc6a48d
Natan Furletti <natan.furletti@outlook>
2023-11-21 23:12:27 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.post("/users", (request, response) => {
+  const { name, email, password } = request.body;
+  response.send(`User: ${name}. E-mail: ${email}. senha ${password}`);
+});
+
+const PORT = 3333;
+app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
