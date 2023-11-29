6171dddf5c10c4573962217239e8cdae5ae0285a
Natan Furletti <natan.furletti@outlook.com>
2023-11-29 15:22:06 -0300

    criação de usuário e encriptografia da senha

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,27 +1,27 @@
-const sqliteConnection = require("../database/sqlite");
-const AppError = require("../utils/AppError");
+const { hash } = require('bcryptjs')
+const AppError = require('../utils/AppError')
+const sqliteConnection = require('../database/sqlite')
 
 class UsersController {
   async create(request, response) {
-    const { name, email, password } = request.body;
+    const { name, email, password } = request.body
 
-    const database = await sqliteConnection();
-    const checkUserExists = await database.get(
-      "SELECT * FROM users WHERE email = (?)",
-      [email]
-    );
+    const database = await sqliteConnection()
+    const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if (checkUserExists) {
-      throw new AppError("Este e-mail já está em uso.");
+    if (checkUserExists){
+      throw new AppError("Este e-mail já está em uso.")
     }
+
+    const hashedPassword = await hash(password, 8)
 
     await database.run(
       "INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
-      [name, email, password]
-    );
+      [name, email, hashedPassword]
+    )
 
-    return response.status(201).json();
+    return response.status(201).json()
   }
 }
 
-module.exports = UsersController;
+module.exports = UsersController
